c709a8695cca659c7287ade55bd8df3019a66aa5
Matt McMeans <mrmcmans@memphis.edu>
2024-03-24 21:08:49 -0500

    fix

--- obj/Debug/net8.0/Menu.AssemblyInfo.cs
+++ obj/Debug/net8.0/Menu.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Menu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ffcbfdcf81d9735ba1896ac4157ea8be74c1b30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+136977c1b4ee29e73d7c6e34bdf347b9f985bd25")]
 [assembly: System.Reflection.AssemblyProductAttribute("Menu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Menu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
